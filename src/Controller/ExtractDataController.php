373f8bbc2496e9edf0971226362b0e7b0a4c9c21
sbarry <bsidy01@gmail.com>
2021-10-28 09:56:30 +0200

    commit opnc du 28/10/2021 09h50 Ajout du controller extraData pour extraire des données pour remplir des selects

--- src/Controller/ExtractDataController.php
+++ src/Controller/ExtractDataController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller;
 
+use App\Repository\SourcesMisesAJourRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class ExtractDataController extends AbstractController
 {
     #[Route('/extract/sourcemaj', name: 'extract_maj')]
-    public function data(): Response
+    public function datamaj(SourcesMisesAJourRepository $misesAJourRepo): Response
     {
 
-
-        return $this->render('extract_data/index.html.twig', [
-            'controller_name' => 'ExtractDataController',
-        ]);
+       $dataSourceMAJ= $misesAJourRepo->findAll();
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $serializer=new Serializer($normalizers,$encoders);
+        // on convertit en json
+        $jsonContent = $serializer->serialize($dataSourceMAJ, 'json',[
+            'circular_reference_handler'=>function($object){   return $object->getId();
+            } ]);
+        $response= new Response($jsonContent);
+        $response->headers->set('Content-Type', 'application/json');
+        // Allow all websites
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
     }
 }
